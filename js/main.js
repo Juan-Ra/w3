d41cee1d2fdc79617b243d83b5906af390d21231
JuanRa <jrrustarazo@gmail.com>
2019-02-12 17:17:57 +0100

    añadido fondo al desplegar el menú oculto

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+let toggler = document.getElementById('toggler');
+toggler.addEventListener('click', toggleBackground);
+
+
+function toggleBackground(){
+
+  let header = document.getElementById('main-header');
+  let menu = document.getElementById('navbar');
+
+  if (menu.className == 'navbar-collapse collapse show'){
+    header.classList.remove('bg-toggle');
+  } else {
+    header.classList.add('bg-toggle');
+  }
+}
+
 window.onscroll = function (){
   let header = document.getElementById('main-header');
   let logo = document.getElementById('logo');
